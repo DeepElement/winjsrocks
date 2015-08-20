52459b69229e3c7521c29a437f417e8820fb3c28
Todd Morrison <todd@deepelement.com>
2015-08-20 07:44:00 -0700

    Added more support for processing calls

--- src/binding/template.js
+++ src/binding/template.js
@@ -1,7 +1,10 @@
-var ioc = require('../ioc');
+var ioc = require('../ioc'),
+  winjsHelper = require('../helper/winjs');
 
 exports.itemTemplateSelector = {
   get: function() {
-    return ioc.getProvider("template").itemTemplateSelector;
+    var instance = ioc.getProvider("template").itemTemplateSelector;
+    winjsHelper.markForProcessing(instance);
+    return instance;
   }
 }
