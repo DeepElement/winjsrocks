a0d23e9098fb3f4bfcebe1fc722e80a66336ae04
Todd Morrison <todd@deepelement.com>
2015-08-20 07:41:45 -0700

    cleanup of markforprocessing api

--- src/helper/winjs.js
+++ src/helper/winjs.js
@@ -1,4 +1,5 @@
-var ioc = require("../ioc");
+var ioc = require("../ioc"),
+  WinJS = require('winjs');
 
 exports.pageDefine = function(viewKey, templateUri, baseClassDef) {
   var existingBaseClass = WinJS.UI.Pages.define(templateUri);
@@ -8,4 +9,17 @@ exports.pageDefine = function(viewKey, templateUri, baseClassDef) {
     return extendedClassDef;
   }
   return existingBaseClass;
+};
+
+exports.markForProcessing = function(subject) {
+  var _self = this;
+  for (var _property in subject)
+    if (subject.hasOwnProperty(_property)) {
+      if (typeof subject[_property] == "object" && _property[0] != "_") {
+        exports.markForProcessing(subject[_property]);
+      } else
+      if (subject[_property] instanceof Function && !subject[_property]["supportedForProcessing"]) {
+        WinJS.Utilities.markSupportedForProcessing(subject[_property]);
+      }
+    }
 };
