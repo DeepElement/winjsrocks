454f20676a3c7aca0f7aa640386b04aca1eff26c
Todd Morrison <todd@deepelement.com>
2015-08-20 15:34:27 -0700

    Update runtime.js

--- src/runtime.js
+++ src/runtime.js
@@ -45,3 +45,11 @@ String.prototype.replaceAll = function(search, replace) {
   }
   return this.split(search).join(replace);
 };
+
+Math.getRandomArbitrary = function(min, max) {
+    return Math.random() * (max - min) + min;
+};
+
+Math.getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
