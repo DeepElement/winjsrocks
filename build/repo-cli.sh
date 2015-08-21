1049d57252f7a80487c942095916de65339cd7fd
Todd Morrison <todd@deepelement.com>
2015-08-20 19:18:28 -0700

    Added post stable build tags

--- build/repo-cli.sh
+++ build/repo-cli.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-  # Fetch repo
-  git clone --quiet https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} travis-build > /dev/null;
-  cd travis-build
 
+git clone --quiet https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} travis-build > /dev/null;
+cd travis-build
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
   # Reverse Integrate
   git checkout master > /dev/null;
   git merge --squash origin/stable --no-commit  > /dev/null;
@@ -15,14 +14,16 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
   git merge origin/master --no-commit  > /dev/null;
   git commit -m "Travis CI auto-merge from travis build ${TRAVIS_BUILD_NUMBER}  - [ci skip]";
   npm version patch -m "Travis CI auto-build from travis build ${TRAVIS_BUILD_NUMBER}";
+
+  git push -q origin stable > /dev/null;
+fi
+if [ "${TRAVIS_BRANCH}" = "stable" ]; then
+  git checkout --track -b stable origin/stable > /dev/null;
   version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
   if [ "$version" != "" ]; then
       git tag -a "v$version" -m "`git log -1 --format=%s`"
       echo "Created a new tag, v$version"
   fi
-
-  git push -q origin stable > /dev/null;
   git push origin --tags > /dev/null;
-
-  cd ..;
 fi
+cd ..;
