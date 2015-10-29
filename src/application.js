16f9b291da78f86fcd80648bbfdb0bf5ae506753
Todd Morrison <todd@deepelement.com>
2015-10-28 17:46:27 -0700

    Added better app api

--- src/application.js
+++ src/application.js
@@ -1,13 +1,45 @@
 import LifeCycle from "./common/lifecycle";
-import ioc from "./ioc";
+import Container from "./ioc";
 import async from "async";
+import Logging from "./log";
+import Configuration from "./config";
+import Builder from "./builder";
 
 export default class extends LifeCycle {
   constructor() {
     super();
+
+    // TODO: make instance based
+    this._logger = Logging;
+
+    // TODO: make instance based
+    this._container = Container;
+
+    // make instance based
+    this._builder = Builder;
+
+    // TODO: make instance based
+    this._configuration = Configuration;
+  }
+
+  get container() {
+    return this._container;
+  }
+
+  get logger() {
+    return this._logger;
+  }
+
+  get builder() {
+    return this._builder;
+  }
+
+  get configuration() {
+    return this._configuration;
   }
 
   configure(options, done) {
+    var that = this;
     options = options || {};
     async.waterfall([
         function(cb) {
@@ -35,33 +67,34 @@ export default class extends LifeCycle {
           return done(err);
 
         // setup the framework services/providers
-        ioc.registerProvider("lokiStorage", require('./provider/loki-storage'));
-        ioc.registerProvider("localStorage", require('./provider/local-storage'));
+        that.container.registerProvider("lokiStorage", require('./provider/loki-storage'));
+        that.container.registerProvider("localStorage", require('./provider/local-storage'));
 
-        ioc.registerService("navigation", require('./service/navigation'));
-        ioc.registerService("message", require('./service/message'));
-        ioc.registerService("application", require('./service/application'));
-        ioc.registerService("data", require('./service/data'));
+        that.container.registerService("navigation", require('./service/navigation'));
+        that.container.registerService("message", require('./service/message'));
+        that.container.registerService("application", require('./service/application'));
+        that.container.registerService("data", require('./service/data'));
 
         return done();
       });
   }
 
   load(options, done) {
+    var that = this;
     super.load(options, function(err) {
       if (err)
         return done(err);
       var messageHooks = options.messageHooks || {};
-      async.each(ioc.getServiceKeys(),
+      async.each(that.container.getServiceKeys(),
         function(key, keyCb) {
-          var serviceInstance = ioc.getService(key);
+          var serviceInstance = that.container.getService(key);
           serviceInstance.start({}).done(keyCb);
         },
         function(err) {
           if (err)
             return done(err);
 
-          var MessageService = ioc.getService("message");
+          var MessageService = that.container.getService("message");
           for (var handler in messageHooks)
             MessageService.register(handler, messageHooks[handler]);
 
@@ -78,13 +111,14 @@ export default class extends LifeCycle {
   }
 
   unload(options, done) {
+    var that = this;
     super.unload(options, function(err) {
       if (err)
         return done(err);
 
-      async.each(ioc.getServiceKeys(),
+      async.each(that.container.getServiceKeys(),
         function(key, keyCb) {
-          var serviceInstance = ioc.getService(key);
+          var serviceInstance = that.container.getService(key);
           serviceInstance.stop({}).done(keyCb);
         },
         function(err) {
@@ -96,13 +130,14 @@ export default class extends LifeCycle {
   }
 
   pause(options, done) {
+    var that = this;
     super.pause(options, function(err) {
       if (err)
         return done(err);
 
-      async.each(ioc.getServiceKeys(),
+      async.each(that.container.getServiceKeys(),
         function(key, keyCb) {
-          var serviceInstance = ioc.getService(key);
+          var serviceInstance = that.container.getService(key);
           serviceInstance.pause({}).done(keyCb);
         },
         function(err) {
@@ -114,12 +149,13 @@ export default class extends LifeCycle {
   }
 
   resume(options, done) {
+    var that = this;
     super.resume(options, function(err) {
       if (err)
         return done(err);
-      async.each(ioc.getServiceKeys(),
+      async.each(that.container.getServiceKeys(),
         function(key, keyCb) {
-          var serviceInstance = ioc.getService(key);
+          var serviceInstance = that.container.getService(key);
           serviceInstance.resume({}).done(keyCb);
         },
         function(err) {
--- src/entry.js
+++ src/entry.js
@@ -1,65 +1,58 @@
 import "./runtime";
 import "./winjs.shim";
 
-import application from "./application";
-import container from "./ioc";
-import logger from "./log";
-import configuration from "./config";
-import baseViewModel from "./view-model/base";
-import itemViewModel from "./view-model/item";
-import baseView from "./view/base";
-import controlView from "./view/control";
-import pageView from "./view/page";
-import baseService from "./service/base";
-import baseProvider from "./provider/base";
-import basePlugin from "./plugin/base";
-import baseModel from "./model/base";
-import baseCommand from "./command/base";
-import builder from "./builder";
+import Application from "./application";
+import BaseViewModel from "./view-model/base";
+import ItemViewModel from "./view-model/item";
+import BaseView from "./view/base";
+import ControlView from "./view/control";
+import PageView from "./view/page";
+import BaseService from "./service/base";
+import BaseProvider from "./provider/base";
+import BasePlugin from "./plugin/base";
+import BaseModel from "./model/base";
+import BaseCommand from "./command/base";
+import Builder from "./builder";
 
-var viewModel = {
-  baseViewModel,
-  itemViewModel
+var ViewModel = {
+  Base: BaseViewModel,
+  Item: ItemViewModel
 }
 
-var view = {
-  baseView,
-  controlView,
-  pageView
+var View = {
+  Base: BaseView,
+  Control: ControlView,
+  Page: PageView
 }
 
-var service = {
-  baseService
+var Service = {
+  Base: BaseService
 }
 
-var provider ={
-  baseProvider
+var Provider ={
+  Base: BaseProvider
 }
 
-var plugin = {
-  basePlugin
+var Plugin = {
+  Base: BasePlugin
 }
 
-var model= {
-  baseModel
+var Model= {
+  Base: BaseModel
 }
 
-var command = {
-  baseCommand
+var Command = {
+  Base: BaseCommand
 }
 
 // The surface API
 export default {
-  application,
-  builder,
-  container,
-  logger,
-  configuration,
-  viewModel,
-  view,
-  service,
-  provider,
-  plugin,
-  model,
-  command
+  Application,
+  ViewModel,
+  View,
+  Service,
+  Provider,
+  Plugin,
+  Model,
+  Command
 }
