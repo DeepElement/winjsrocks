fd4f0e42878ce5c3d67aefe912b56b1260722465
Todd Morrison <todd@deepelement.com>
2015-11-01 09:50:14 -0800

    Convertion of Command base to ES6

--- src/command/base.js
+++ src/command/base.js
@@ -1,36 +1,17 @@
-var WinJS = require('winjs'),
-  mixins = require('../helper/mixins');
+import Eventable from "../common/eventable";
 
-// RelayCommand Constructor
-// ----------------------
-function _constructor(payload, key) {
-  var _self = this;
-  _self._key = key;
-  _self._internalExecute = payload || function() {};
-  _self.execute = function() {
-    return _self._internalExecute.apply(_self, arguments);
-  };
-  _self.canExecute = true;
-}
-
-// RelayCommand Methods
-// ------------------
-var instanceMembers = {
-  execute: null,
-  canExecute: null,
-  application: {
-    get: function() {
-      return this._application;
-    },
-    set: function(value) {
-      this._application = value;
-    }
+export default class extends Eventable {
+  constructor(payload, key) {
+    super();
+    this._key = key;
+    this._internalExecute = payload || function() {};
   }
-};
 
-var staticMembers = {};
+  execute() {
+    return this._internalExecute.apply(this, arguments);
+  }
 
-module.exports = WinJS.Class.define(_constructor,
-  instanceMembers, staticMembers);
-WinJS.Class.mix(module.exports, WinJS.Utilities.eventMixin);
-WinJS.Class.mix(module.exports, mixins.notify);
+  get canExecute() {
+    return this._canExecute;
+  }
+}
