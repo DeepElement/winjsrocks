e46a7a9c520bb5d62bb020f57dbb48ebf611c28c
Todd Morrison <todd@deepelement.com>
2015-11-01 21:55:01 -0800

    Hardcode stable winjs version and removed old gulp-file deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,9 @@
 var gulp = require('gulp'),
   mocha = require('gulp-mocha'),
-  async = require('async'),
-  source = require('vinyl-source-stream'),
   runSequence = require('gulp-run-sequence'),
-  path = require('path'),
-  glob = require("glob"),
   rimraf = require('rimraf'),
   babel = require('babel-core/register'),
-  fs = require('fs-extra'),
-  mkdirp = require('mkdirp'),
-  exec = require('child_process').exec;
+  fs = require('fs-extra');
 
 var gutil = require("gulp-util"),
   webpack = require("webpack"),
