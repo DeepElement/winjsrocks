156919d3ebf81ea4769cee93b7207e2cb692adab
Todd Morrison <todd@deepelement.com>
2015-11-03 13:43:00 -0800

    BugFix for typo in item template selector

--- src/binding/template.js
+++ src/binding/template.js
@@ -7,14 +7,15 @@ export default class extends Component {
   }
 
   ItemTemplateSelector(itemPromise) {
+    var that = this;
     var mediaTile = document.createElement("div");
     WinJS.Utilities.addClass(mediaTile, "item-template");
     var _renderCompletePromise = itemPromise.then(function(item) {
       var itemViewModel = item instanceof Array ? item[0].data : item.data;
-      var viewKey = itemViewModel.item.contenType.toLowerCase();
+      var viewKey = itemViewModel.item.contentType.toLowerCase();
       var viewTemplateUri = config.get("domain:" + viewKey + ":template");
       WinJS.Utilities.addClass(mediaTile, "item-template-" + viewKey);
-      var viewClassDef = ioc.getItemViewDef(viewKey);
+      var viewClassDef = that.application.container.getItemViewDef(viewKey);
       winjsHelper.pageDefine(viewKey, viewTemplateUri, viewClassDef);
       return WinJS.UI.Pages.render(viewTemplateUri, mediaTile, itemViewModel);
     });
