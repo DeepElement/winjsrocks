674ac1cb0223911a2940f5ada4a6c8f9371f6b53
Todd Morrison <todd@deepelement.com>
2015-11-06 10:20:16 -0800

    Plugin constructor removal of scope identifiers

--- src/plugin/base.js
+++ src/plugin/base.js
@@ -1,43 +1,18 @@
 import LifeCycle from "../common/lifecycle"
 
 export default class extends LifeCycle {
-  constructor(application, key, type) {
+  constructor(application, key) {
     super(application);
-    var validTypes = ['service', 'provider', 'runtime'];
-    type = type || 'runtime';
     if (!key)
       throw new Error("Key is required for a plugin.");
-    if (!type)
-      throw new Error("Type is required for a Plugin. Valid values are 'service', 'provider', 'runtime'");
     this._key = key;
-    this._pluginType = type;
   }
 
   get key() {
     return this._key;
   }
 
-  get type() {
-    return this._pluginType;
-  }
-
   loadComponent(options, callback) {
-    var that = this;
-
-    // register self
-    switch (that.type) {
-      case "provider":
-        // TODO: register self instance as a provider
-        that.application.container.registerProvider(that.key, that.constructor, "request");
-        that.application.container.addInstance(that.key, this);
-        break;
-      case "service":
-        // TODO: register self instance as a service
-        that.application.container.registerService(that.key, that.constructor, "application");
-        that.application.container.addInstance(that.key, this);
-        break;
-    }
-
     return super.loadComponent(options, callback);
   }
 };
