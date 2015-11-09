abb9e33d5474b69db03c5deaa936dc8ab7edbc5a
Mr-Robot <todd@deepelement.com>
2015-11-09 03:43:50 +0000

    Travis CI auto-merge of master from build 839 - [ci skip]

--- build/repo-cli.sh
+++ build/repo-cli.sh
@@ -7,50 +7,43 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
   # Forward Integrate
   git checkout --track -b integration origin/integration > /dev/null;
   git merge --squash origin/master --no-commit  > /dev/null;
-  git commit -m "Travis CI auto-merge of master from build ${TRAVIS_BUILD_NUMBER}";
+  git commit -m "Travis CI auto-merge of master from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+  npm version patch -m "Travis CI auto-version from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
   git push -q origin integration > /dev/null;
+  git push origin --tags;
+
+  # update local
+  git fetch;
+
+  # Reverse Integrate
+  git checkout master > /dev/null;
+  git merge --squash origin/integration --no-commit  > /dev/null;
+  git commit -m "Travis CI auto-merge of integration from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+  git push -q origin master > /dev/null;
 
 fi
 
-if [ "${TRAVIS_BRANCH}" = "integration" ]; then
-  if [[ -z "$TRAVIS_TAG" ]]; then
-
-    # Tag for Release
-    git checkout --track -b integration origin/integration > /dev/null;
-    npm version patch -m "Travis CI auto-version from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
-    git push origin integration;
-    git push origin --tags;
-
-    # Reverse Integrate
-    git checkout master > /dev/null;
-    git merge --squash origin/integration --no-commit  > /dev/null;
-    git commit -m "Travis CI auto-merge of integration from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
-    git push -q origin master > /dev/null;
-
-  fi
-fi
-
-if [ "${TRAVIS_BRANCH}" = "stable" ]; then
-  if [[ -z "$TRAVIS_TAG" ]]; then
-
-    # Tag for Release
-    git checkout --track -b stable origin/stable > /dev/null;
-    npm version minor -m "Travis CI auto-version from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
-    git push origin stable;
-    git push origin --tags;
-
-    # Reverse Integrate
-    git checkout --track -b integration origin/integration > /dev/null;
-    git merge --squash origin/stable --no-commit  > /dev/null;
-    git commit -m "Travis CI auto-merge of stable from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
-    git push -q origin integration > /dev/null;
-
-    # Reverse Integrate
-    git checkout master > /dev/null;
-    git merge --squash origin/stable --no-commit  > /dev/null;
-    git commit -m "Travis CI auto-merge of stable from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
-    git push -q origin master > /dev/null;
-
-  fi
-fi
+#if [ "${TRAVIS_BRANCH}" = "stable" ]; then
+#  if [[ -z "$TRAVIS_TAG" ]]; then
+#
+#    # Tag for Release
+#    git checkout --track -b stable origin/stable > /dev/null;
+#    npm version minor -m "Travis CI auto-version from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+#    git push origin stable;
+#    git push origin --tags;
+#
+#    # Reverse Integrate
+#    git checkout --track -b integration origin/integration > /dev/null;
+#    git merge --squash origin/stable --no-commit  > /dev/null;
+#    git commit -m "Travis CI auto-merge of stable from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+#    git push -q origin integration > /dev/null;
+#
+#    # Reverse Integrate
+#    git checkout master > /dev/null;
+#    git merge --squash origin/stable --no-commit  > /dev/null;
+#    git commit -m "Travis CI auto-merge of stable from build ${TRAVIS_BUILD_NUMBER} - [ci skip]";
+#    git push -q origin master > /dev/null;
+#
+#  fi
+#fi
 cd ..;
